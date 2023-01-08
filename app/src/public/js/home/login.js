ac11f02d401aa51811c9d584332a20cbcbc6b544
hjlee810 <leehyejin940810@gmail.com>
2023-01-08 16:34:39 +0900

    프런트 로그인 기능 구현

--- app/src/public/js/home/login.js
+++ app/src/public/js/home/login.js
@@ -20,5 +20,11 @@ function login(){
     body : JSON.stringify(req),
   })
   .then((res)=>res.json())
-  .then(console.log);
+  .then((res)=>{
+    if(res.success){
+      location.href="/";
+    } else {
+      alert(res.msg);
+    }
+  });
 }
