6b7b861e711b9af056849f296de2058c79227a2f
hjlee810 <leehyejin940810@gmail.com>
2023-02-01 07:12:44 +0900

    회원가입 라우팅 구현

--- app/src/models/User.js
+++ app/src/models/User.js
@@ -7,17 +7,21 @@ class User{
     this.body = body;
   }
   login(){
-    const body = this.body
-    const {id, psword} = UserStorage.getUserinfo(body.id);
+    const client = this.body
+    const {id, psword} = UserStorage.getUserinfo(client.id);
   
     if(id){
-      if(id === body.id && psword === body.psword){
+      if(id === client.id && psword === client.psword){
         return {success : true}
       }
       return {success : false, msg : "비밀번호가 일치하지 않습니다."}
     }
     return {success : false, msg : "존재하지 않는 아이디입니다."}
-
+  }
+  register(){
+    const client = this.body;
+    const response = UserStorage.save(client);
+    return response;
   }
   
 }
